3ff266eaed755cf943c97f6fc43a88fbfb170165
Wilmer Mendoza <wil13ale@hotmail.com>
2016-07-18 23:56:17 -0400

    Tarea terminada lista para subir a github

--- Tarea.playground/Contents.swift
+++ Tarea.playground/Contents.swift
@@ -1,4 +1,37 @@
-//: Playground - noun: a place where people can play
+//wilmer mendoza
+//tarea semana 2
 
 import UIKit
+
+var rango = 0...100
+
+for num in rango {
+    if num % 2 == 0 {
+        print("#\(num) es par")
+        if num % 5 == 0 {
+            print("#\(num) Bingo!!!")
+            
+            if num >= 30 && num <= 40 {
+                print("#\(num) Viva Swift")
+            }
+        
+        }else if num >= 30 && num <= 40 {
+            print("#\(num) Viva Swift")
+        }
+        
+    
+    }else{
+        print("#\(num) es impar")
+        if num % 5 == 0 {
+            print("#\(num) Bingo!!!")
+            if num >= 30 && num <= 40 {
+                print("#\(num) Viva Swift")
+            }
+            
+        }else if num >= 30 && num <= 40 {
+            print("#\(num) Viva Swift")
+        }
+    }
+
+}
 
